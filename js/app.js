3171f1e16021093119a8bd309bb6a193284e6ee8
pxuria <pouriamesram@gmail.com>
2024-07-04 10:01:45 -0700

    fix slider

--- js/app.js
+++ js/app.js
@@ -12,8 +12,29 @@ let formStepsNum = 0;
 
 // packages
 var swiper = new Swiper(".mySwiper", {
-  effect: "cards",
+  effect: "coverflow",
   grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  initialSlide: 1,
+  speed: 600,
+  preventClicks: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 80,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true,
+  },
+  on: {
+    click(event) {
+      swiper.slideTo(this.clickedIndex);
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
 
 // eventlisteners
